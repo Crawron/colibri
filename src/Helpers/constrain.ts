509a312f4ae4c3699792b8fbacba10e3762f32eb
Crawron <azertyaaron@gmail.com>
2018-05-19 20:54:55 -0500

    added a constrain function

---
+++ src/Helpers/constrain.ts
@@ -0,0 +1,7 @@
+import { NumberRange } from "../Classes/Range"
+
+export function constrain(value: number, range:NumberRange) {
+    if (value < range.start) return range.start
+    if (value > range.end) return range.end
+    return range
+}
