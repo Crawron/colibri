354d35d206a6e3e98486569f3618b6d99b69ab81
Crawron <azertyaaron@gmail.com>
2018-05-19 23:57:56 -0500

    Added map functions Still missing the color map

---
+++ src/Helpers/map.ts
@@ -0,0 +1,27 @@
+import { Color } from 'three'
+import { NumberRange } from '../Classes/Range'
+import { constrain } from './constrain';
+
+export function map(originRange: NumberRange, targetRange: NumberRange, value: number, clamp?: boolean) {
+    value -= originRange.start
+
+    let result = value * targetRange.length / originRange.length
+    result += targetRange.start
+
+    if (clamp) {
+        if (targetRange.length >= 0) 
+            result = constrain(result, targetRange)
+        else 
+            result = constrain(result, targetRange.inverse)
+    }
+
+    return result
+}
+
+export function normalize(range: NumberRange, value: number) {
+    return map(range, new NumberRange(0,1), value)
+}
+
+export function denormalize(value: number, range: NumberRange) {
+    return map(new NumberRange(0,1), range, value)
+}
