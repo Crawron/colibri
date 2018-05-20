515b94c7d33539545a39134a9da62a43bf8bcb24
Crawron <azertyaaron@gmail.com>
2018-05-19 23:57:12 -0500

    uhhh wrong return in constrain, fixed

--- src/Helpers/constrain.ts
+++ src/Helpers/constrain.ts
@@ -3,5 +3,5 @@ import { NumberRange } from "../Classes/Range"
 export function constrain(value: number, range:NumberRange) {
     if (value < range.start) return range.start
     if (value > range.end) return range.end
-    return range
+    return value
 }
